7828cf6e36908d8e87cb6951be24b5af1f424341
Jeremy D. Impson <github@ur.selfip.net>
2024-02-11 13:09:51 -0500

    improve automation

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-docker build . -t jdimpson/openvpn-client
+IMAGE='jdimpson/openvpn-client';
+
+if docker images | grep -q "$IMAGE"; then
+	docker image rm "$IMAGE";
+fi
+
+docker build . -t "$IMAGE"
