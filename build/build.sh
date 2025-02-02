50466db958a27672c7f486fbf1618240536acbb0
Jeremy D. Impson <github@echo3.info>
2025-02-01 21:29:48 -0500

    update build script

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-
-IMAGE='jdimpson/openvpn-client';
+set -e
+IMAGE=$(cat IMAGENAME);
+if test -z "$IMAGE"; then
+	echo "Problem reading image name from file IMAGENAME";
+	exit 1;
+fi
 
 if docker images | grep -q "$IMAGE"; then
 	docker image rm "$IMAGE";
 fi
 
 docker build . -t "$IMAGE"
+echo docker image push $IMAGE
